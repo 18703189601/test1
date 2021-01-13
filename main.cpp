09f75e70c484d91efab6862d211babaa1cebdbf6
lmx_pro <2899627516@qq.com>
2021-01-13 19:09:17 +0800

    My second commit

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int main()
 {
+/*
     doublyList<int> L;
     L.push_front(3);
     L.push_front(4);
@@ -30,5 +31,6 @@ int main()
     cout << "************************" << endl;
     L.output(L.length(), false);
     cout << "The length of the list is: " << L.length() << endl;
+*/
     return 0;
 }
